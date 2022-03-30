6cf788f120523c1cf97176b3333bf6d20eba8353
brennanngo992k <brennanngo992k@gmail.com>
2022-03-31 05:27:39 +0700

    how to use bloc for mutil screen

---
+++ lib/presentation/screens/first_screen.dart
@@ -0,0 +1,90 @@
+import 'package:bloc_example/logic/cubits/counter_cubit/counter_cubit.dart';
+import 'package:bloc_example/presentation/screens/second_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // BlocListener<CounterCubit, CounterState>(
+            //   listener: (context, state) {
+            //     if (state.isIncrement == true) {
+            //       ScaffoldMessenger.of(context).showSnackBar(
+            //           SnackBar(content: Text('INCREMENT ${state.counter}')));
+            //     } else if (state.isIncrement == false) {
+            //       ScaffoldMessenger.of(context).showSnackBar(
+            //           SnackBar(content: Text('DECREMENT ${state.counter}')));
+            //     }
+            //   },
+            //   child: Text(
+            //     'VALUE',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   ),
+            // ),
+            // BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
+            //   return Text(
+            //     '${state.counter}',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   );
+            // }),
+            BlocConsumer<CounterCubit, CounterState>(builder: (context, state) {
+              return Text(
+                '${state.counter}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }, listener: (context, state) {
+              if (state.isIncrement == true) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('INCREMENT ${state.counter}'),
+                    duration: const Duration(microseconds: 10),
+                  ),
+                );
+              } else if (state.isIncrement == false) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('DECREMENT ${state.counter}'),
+                  duration: const Duration(microseconds: 10),
+                ));
+              }
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                    onPressed: BlocProvider.of<CounterCubit>(context).decrement,
+                    child: const Icon(Icons.subway_outlined)),
+                TextButton(
+                    onPressed: BlocProvider.of<CounterCubit>(context).increment,
+                    child: const Icon(Icons.add_outlined))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const SecondScreen(title: 'Screen 2'))),
+                child: const Text('Go to second screen'))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/second_screen.dart
@@ -0,0 +1,85 @@
+import 'package:bloc_example/logic/cubits/counter_cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // BlocListener<CounterCubit, CounterState>(
+            //   listener: (context, state) {
+            //     if (state.isIncrement == true) {
+            //       ScaffoldMessenger.of(context).showSnackBar(
+            //           SnackBar(content: Text('INCREMENT ${state.counter}')));
+            //     } else if (state.isIncrement == false) {
+            //       ScaffoldMessenger.of(context).showSnackBar(
+            //           SnackBar(content: Text('DECREMENT ${state.counter}')));
+            //     }
+            //   },
+            //   child: Text(
+            //     'VALUE',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   ),
+            // ),
+            // BlocBuilder<CounterCubit, CounterState>(builder: (context, state) {
+            //   return Text(
+            //     '${state.counter}',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   );
+            // }),
+            BlocConsumer<CounterCubit, CounterState>(builder: (context, state) {
+              return Text(
+                '${state.counter}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }, listener: (context, state) {
+              if (state.isIncrement == true) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('INCREMENT ${state.counter}'),
+                    duration: const Duration(microseconds: 10),
+                  ),
+                );
+              } else if (state.isIncrement == false) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('DECREMENT ${state.counter}'),
+                  duration: const Duration(microseconds: 10),
+                ));
+              }
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                    onPressed: BlocProvider.of<CounterCubit>(context).decrement,
+                    child: const Icon(Icons.subway_outlined)),
+                TextButton(
+                    onPressed: BlocProvider.of<CounterCubit>(context).increment,
+                    child: const Icon(Icons.add_outlined))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Go back'))
+          ],
+        ),
+      ),
+    );
+  }
+}
