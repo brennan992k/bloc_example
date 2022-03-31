c86c340500d527c2df24b1a2db728f50dc3dfc96
brennanngo992k <brennanngo992k@gmail.com>
2022-03-31 22:14:10 +0700

    how to use context.select <== like use select in redux ==>, context.watch <== like blocbuilder or bloccustomer ==>, context.read <== like bloclistener ==>

--- lib/presentation/screens/first_screen.dart
+++ lib/presentation/screens/first_screen.dart
@@ -45,17 +45,71 @@ class _FirstScreenState extends State<FirstScreen> {
             //     style: Theme.of(context).textTheme.headline4,
             //   );
             // }),
-            BlocConsumer<InternetCubit, InternetState>(
-                builder: (context, state) {
-              if (state is InternetDisconnected) {
+            // BlocConsumer<InternetCubit, InternetState>(
+            //     builder: (context, state) {
+            //   if (state is InternetDisconnected) {
+            //     return Text(
+            //       'Disconnected',
+            //       style: Theme.of(context).textTheme.headline4,
+            //     );
+            //   }
+            //   if (state is InternetConnected) {
+            //     return Text(
+            //       'Connected',
+            //       style: Theme.of(context).textTheme.headline4,
+            //     );
+            //   }
+            //   return Text(
+            //     'Connectting',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   );
+            // }, listener: (context, state) {
+            //   // if (state.isIncrement == true) {
+            //   //   ScaffoldMessenger.of(context).showSnackBar(
+            //   //     SnackBar(
+            //   //       content: Text('INCREMENT ${state.counter}'),
+            //   //       duration: const Duration(microseconds: 10),
+            //   //     ),
+            //   //   );
+            //   // } else if (state.isIncrement == false) {
+            //   //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            //   //     content: Text('DECREMENT ${state.counter}'),
+            //   //     duration: const Duration(microseconds: 10),
+            //   //   ));
+            //   // }
+            // }),
+            // BlocConsumer<CounterCubit, CounterState>(builder: (context, state) {
+            //   return Text(
+            //     '${state.counter}',
+            //     style: Theme.of(context).textTheme.headline4,
+            //   );
+            // }, listener: (context, state) {
+            //   // if (state.isIncrement == true) {
+            //   //   ScaffoldMessenger.of(context).showSnackBar(
+            //   //     SnackBar(
+            //   //       content: Text('INCREMENT ${state.counter}'),
+            //   //       duration: const Duration(microseconds: 10),
+            //   //     ),
+            //   //   );
+            //   // } else if (state.isIncrement == false) {
+            //   //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            //   //     content: Text('DECREMENT ${state.counter}'),
+            //   //     duration: const Duration(microseconds: 10),
+            //   //   ));
+            //   // }
+            // }),
+
+            Builder(builder: (context) {
+              final internetCubit = context.watch<InternetCubit>();
+              if (internetCubit.state is InternetConnected) {
                 return Text(
-                  'Disconnected',
+                  'Connected',
                   style: Theme.of(context).textTheme.headline4,
                 );
               }
-              if (state is InternetConnected) {
+              if (internetCubit.state is InternetDisconnected) {
                 return Text(
-                  'Connected',
+                  'Disconnected',
                   style: Theme.of(context).textTheme.headline4,
                 );
               }
@@ -63,49 +117,23 @@ class _FirstScreenState extends State<FirstScreen> {
                 'Connectting',
                 style: Theme.of(context).textTheme.headline4,
               );
-            }, listener: (context, state) {
-              // if (state.isIncrement == true) {
-              //   ScaffoldMessenger.of(context).showSnackBar(
-              //     SnackBar(
-              //       content: Text('INCREMENT ${state.counter}'),
-              //       duration: const Duration(microseconds: 10),
-              //     ),
-              //   );
-              // } else if (state.isIncrement == false) {
-              //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              //     content: Text('DECREMENT ${state.counter}'),
-              //     duration: const Duration(microseconds: 10),
-              //   ));
-              // }
             }),
-            BlocConsumer<CounterCubit, CounterState>(builder: (context, state) {
-              return Text(
-                '${state.counter}',
-                style: Theme.of(context).textTheme.headline4,
-              );
-            }, listener: (context, state) {
-              // if (state.isIncrement == true) {
-              //   ScaffoldMessenger.of(context).showSnackBar(
-              //     SnackBar(
-              //       content: Text('INCREMENT ${state.counter}'),
-              //       duration: const Duration(microseconds: 10),
-              //     ),
-              //   );
-              // } else if (state.isIncrement == false) {
-              //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              //     content: Text('DECREMENT ${state.counter}'),
-              //     duration: const Duration(microseconds: 10),
-              //   ));
-              // }
-            }),
+
+            Text(
+              '${context.select<CounterCubit, int>((bloc) => bloc.state.counter)}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 TextButton(
-                    onPressed: BlocProvider.of<CounterCubit>(context).decrement,
+                    //onPressed: BlocProvider.of<CounterCubit>(context).decrement,
+                    onPressed: context.read<CounterCubit>().decrement,
                     child: const Icon(Icons.subway_outlined)),
                 TextButton(
-                    onPressed: BlocProvider.of<CounterCubit>(context).increment,
+                    //onPressed: BlocProvider.of<CounterCubit>(context).increment,
+                    onPressed: context.read<CounterCubit>().increment,
                     child: const Icon(Icons.add_outlined))
               ],
             ),
